f42b8ddad391e648c09d83c16398322a4f2a41a5
zidbih <medmahmoudhdaya@gmail.com>
2025-12-30 19:15:40 +0000

    fix: append middleware to web and api globaly

--- src/DeadlockServiceProvider.php
+++ src/DeadlockServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Http\Kernel;
 use Zidbih\Deadlock\Console\ListDeadlocksCommand;
 use Zidbih\Deadlock\Console\CheckDeadlocksCommand;
+use Zidbih\Deadlock\Middleware\DeadlockGuardMiddleware;
 use Zidbih\Deadlock\Scanner\DeadlockScanner;
 
 final class DeadlockServiceProvider extends ServiceProvider
@@ -20,8 +21,14 @@ final class DeadlockServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->environment('local')) {
-            $this->app->make(Kernel::class)
-                ->pushMiddleware(\Zidbih\Deadlock\Middleware\DeadlockGuardMiddleware::class);
+            $kernel = $this->app->make(Kernel::class);
+
+            foreach (['web', 'api'] as $group) {
+                $kernel->appendMiddlewareToGroup(
+                    $group,
+                    DeadlockGuardMiddleware::class
+                );
+            }
         }
 
         if ($this->app->runningInConsole()) {
